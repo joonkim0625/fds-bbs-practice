546abf50230317ae4a3de180ff4e4e874ec8ed50
Hyunjoon Kim <joonkim0625@gmail.com>
2018-11-06 17:12:05 +0900

    새글 쓰기 기능

--- src/index.js
+++ src/index.js
@@ -73,6 +73,9 @@ async function drawPostList() {
 
   // 2. 요소 선택
   const listEl = frag.querySelector('.post-list')
+  // 새글 요소
+  const createEl = frag.querySelector('.create')
+
   // 3. 필요한 데이터 불러오기
   // {data} -> 분해대입을 사용한 것. data를 꺼내와서 바로 변수로 사용 가능.
   // {data: postList} -> data라는 속성 이름을 가져와서 postList에다가 저장하는 것.
@@ -101,7 +104,9 @@ async function drawPostList() {
   }
   // 5. 이벤트 리스너 등록하기
 
-
+  createEl.addEventListener('click', e => {
+    drawNewPostForm()
+  })
 
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
@@ -196,15 +201,45 @@ async function drawPostDetail(postId) {
   rootEl.appendChild(frag)
 }
 
+// 새글 추가를 위한
 async function drawNewPostForm() {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postForm, true)
   // 2. 요소 선택
+  const formEl = frag.querySelector('.post-form')
+  const backEl = frag.querySelector('.back')
+
   // 3. 필요한 데이터 불러오기
   // 4. 내용 채우기
   // 5. 이벤트 리스너 등록하기
+
+
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const title = e.target.elements.title.value
+    const body = e.target.elements.body.value
+
+    await api.post('/posts', {
+      title,
+      body
+    })
+    drawPostList()
+  })
+
+  backEl.addEventListener("click", e => {
+    // 브라우저에 내재된 버튼의 기본 기능작동을 방지하기 위한 프리벤트 설정.
+    e.preventDefault()
+    drawPostList();
+  });
+
+
+
   // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
+// 글 수정
 async function drawEditPostForm(postId) {
   // 1. 템플릿 복사
   // 2. 요소 선택
